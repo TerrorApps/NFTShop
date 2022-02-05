9ec979cad8b8e4529f77b9d8edd46877b59748b6
Taka Goto <tgoto@8thlight.com>
2022-02-05 00:27:36 -0600

    add error message; fix azuki less than 1000

--- pages/api/azuki.ts
+++ pages/api/azuki.ts
@@ -17,68 +17,68 @@ export default async function handler(
 ) {
   sharp.cache(false);
   var tokenId = req.query["tokenId"]
-  if (tokenId.length < 4) {
-    for (let i = tokenId.length; i < 4; i++) {
-      tokenId = "0" + tokenId
-    }
-  }
-  var fileName = `${tmpdir}/azuki_${tokenId}.png`
-  var imageUrl = `https://ikzttp.mypinata.cloud/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/${tokenId}.png`
-  await Jimp.read(imageUrl, async function (err, image) {
-    console.log("finding image color")
-    var hex = image.getPixelColor(1, 1)
-    var rgb = Jimp.intToRGBA(hex)
-    var input = await axios(
-      {url: imageUrl,
-      responseType: "arraybuffer"})
-    var buffer = input.data as Buffer
-    var oni = await sharp(buffer).resize({
-      fit: sharp.fit.contain,
-      width: 1170,
-      height: 1170,
-      background: { r: 243, g: 243, b: 4 }
-    }).toBuffer()
-    console.log("sharping azuki")
-    var logoFilePath = path.resolve('.', 'assets/azuki_logo.svg')
-    var logo = await sharp(logoFilePath).png().toBuffer()
-    console.log("found logo grey")
-    var bannerFilePath = path.resolve('.', 'assets/0n1-banner-overlay.png')
-    var banner = await sharp(bannerFilePath).resize({
-      fit: sharp.fit.contain,
-      width: 1170
-    }).png().toBuffer()
-    console.log("sharping banner")
-    await sharp({
-      create: {
+  if (Number(tokenId) > 9999 || Number(tokenId) < 1) {
+      console.log("its invalid")
+      res.status(400).end()
+  } else {
+    var fileName = `${tmpdir}/azuki_${tokenId}.png`
+    var imageUrl = `https://ikzttp.mypinata.cloud/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/${tokenId}.png`
+    await Jimp.read(imageUrl, async function (err, image) {
+        console.log("finding image color")
+        var hex = image.getPixelColor(1, 1)
+        var rgb = Jimp.intToRGBA(hex)
+        var input = await axios(
+        {url: imageUrl,
+        responseType: "arraybuffer"})
+        var buffer = input.data as Buffer
+        var oni = await sharp(buffer).resize({
+        fit: sharp.fit.contain,
         width: 1170,
-        height: 2532,
-        channels: 4,
-        background: { r: rgb.r, g: rgb.g, b: rgb.b }
-      }
-    }).png()
-    .composite([{
-      input: oni,
-      top: 1370,
-      left: 0,
-    },
-    {
-      input: logo,
-      top: 750,
-      left: 75
-    }
-    ]).png().toFile(fileName)
+        height: 1170,
+        background: { r: 243, g: 243, b: 4 }
+        }).toBuffer()
+        console.log("sharping azuki")
+        var logoFilePath = path.resolve('.', 'assets/azuki_logo.svg')
+        var logo = await sharp(logoFilePath).png().toBuffer()
+        console.log("found logo grey")
+        var bannerFilePath = path.resolve('.', 'assets/0n1-banner-overlay.png')
+        var banner = await sharp(bannerFilePath).resize({
+        fit: sharp.fit.contain,
+        width: 1170
+        }).png().toBuffer()
+        console.log("sharping banner")
+        await sharp({
+        create: {
+            width: 1170,
+            height: 2532,
+            channels: 4,
+            background: { r: rgb.r, g: rgb.g, b: rgb.b }
+        }
+        }).png()
+        .composite([{
+        input: oni,
+        top: 1370,
+        left: 0,
+        },
+        {
+        input: logo,
+        top: 750,
+        left: 75
+        }
+        ]).png().toFile(fileName)
 
-    console.log("sharp created image")
-    
-    var stat = fileSystem.statSync(fileName);
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': stat.size
-    });
-    console.log("set file content lenght")
-    var readStream = fileSystem.createReadStream(fileName);
-    readStream.pipe(res);
-    console.log("finished file stuff")
-    res.status(200)
-  })
+        console.log("sharp created image")
+        
+        var stat = fileSystem.statSync(fileName);
+        res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': stat.size
+        });
+        console.log("set file content lenght")
+        var readStream = fileSystem.createReadStream(fileName);
+        readStream.pipe(res);
+        console.log("finished file stuff")
+        res.status(200)
+    })
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,22 +8,6 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   var image = null;
   return (
-  // <div className="py-12 bg-white">
-  //   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-  //     <Link href="/0n1">
-  //       <a>0n1</a>
-  //     </Link>
-  //     <Link href="/azuki">
-  //       <a>Azuki</a>
-  //     </Link>
-  //     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  //     0n1
-  //     </button>
-  //     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  //     Azuki
-  //     </button>
-  //   </div>
-  // </div>
   <div className="bg-gray-50">
   <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
     <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">

34467acb43644749770ab17f0191fc2527129fca
Taka Goto <tgoto@8thlight.com>
2022-02-05 01:05:13 -0600

    try download tag

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -22,67 +22,69 @@ export default async function handler(
       tokenId = "0" + tokenId
     }
   }
-  var fileName = `${tmpdir}/0n1_${tokenId}.png`
-  var imageUrl = `https://ipfs.io/ipfs/QmcoavNZq2jyZGe2Zi4nanQqzU9hRPxunHAo8pgYZ5fSep/${tokenId}.png`
-  await Jimp.read(imageUrl, async function (err, image) {
-    console.log("finding image color")
-    var hex = image.getPixelColor(1, 1)
-    var rgb = Jimp.intToRGBA(hex)
-    var input = await axios(
-      {url: imageUrl,
-      responseType: "arraybuffer"})
-    var buffer = input.data as Buffer
-    var oni = await sharp(buffer).resize({
-      fit: sharp.fit.contain,
-      width: 1170,
-      height: 1170,
-      background: { r: 243, g: 243, b: 4 }
-    }).toBuffer()
-    console.log("sharping 0n1")
-    var logoFilePath = path.resolve('.', 'assets/0N1_logo_grey.svg')
-    var logo = await sharp(logoFilePath).png().toBuffer()
-    console.log("found logo grey")
-    var bannerFilePath = path.resolve('.', 'assets/0n1-banner-overlay.png')
-    var banner = await sharp(bannerFilePath).resize({
-      fit: sharp.fit.contain,
-      width: 1170
-    }).png().toBuffer()
-    console.log("sharping banner")
-    await sharp({
-      create: {
+  if (Number(tokenId) > 9999 || Number(tokenId) < 1) {
+    console.log("its invalid")
+    res.status(400).end()
+} else {
+    var fileName = `${tmpdir}/0n1_${tokenId}.png`
+    var imageUrl = `https://ipfs.io/ipfs/QmcoavNZq2jyZGe2Zi4nanQqzU9hRPxunHAo8pgYZ5fSep/${tokenId}.png`
+    var traitsRes = await fetch(`https://ipfs.io/ipfs/QmXgSuLPGuxxRuAana7JdoWmaS25oAcXv3x2pYMN9kVfg3/${tokenId}`)
+    var traitsData = await traitsRes.json()
+    console.log("traits")
+    console.log(traitsData)
+    await Jimp.read(imageUrl, async function (err, image) {
+      var hex = image.getPixelColor(1, 1)
+      var rgb = Jimp.intToRGBA(hex)
+      var input = await axios(
+        {url: imageUrl,
+        responseType: "arraybuffer"})
+      var buffer = input.data as Buffer
+      var oni = await sharp(buffer).resize({
+        fit: sharp.fit.contain,
         width: 1170,
-        height: 2532,
-        channels: 4,
-        background: { r: rgb.r, g: rgb.g, b: rgb.b }
+        height: 1170,
+        background: { r: 243, g: 243, b: 4 }
+      }).toBuffer()
+      var logoFilePath = path.resolve('.', 'assets/0N1_logo_grey.svg')
+      var logo = await sharp(logoFilePath).png().toBuffer()
+      var bannerFilePath = path.resolve('.', 'assets/0n1-banner-overlay.png')
+      var banner = await sharp(bannerFilePath).resize({
+        fit: sharp.fit.contain,
+        width: 1170
+      }).png().toBuffer()
+      await sharp({
+        create: {
+          width: 1170,
+          height: 2532,
+          channels: 4,
+          background: { r: rgb.r, g: rgb.g, b: rgb.b }
+        }
+      }).png()
+      .composite([{
+        input: oni,
+        top: 1370,
+        left: 0,
+      },
+      {
+        input: logo,
+        top: 750,
+        left: 300
+      },
+      {
+        input: banner,
+        gravity: "south"
       }
-    }).png()
-    .composite([{
-      input: oni,
-      top: 1370,
-      left: 0,
-    },
-    {
-      input: logo,
-      top: 750,
-      left: 300
-    },
-    {
-      input: banner,
-      gravity: "south"
-    }
-    ]).png().toFile(fileName)
+      ]).png().toFile(fileName)
 
-    console.log("sharp created image")
-    
-    var stat = fileSystem.statSync(fileName);
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': stat.size
-    });
-    console.log("set file content lenght")
-    var readStream = fileSystem.createReadStream(fileName);
-    readStream.pipe(res);
-    console.log("finished file stuff")
-    res.status(200)
-  })
+      
+      var stat = fileSystem.statSync(fileName);
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': stat.size
+      });
+      var readStream = fileSystem.createReadStream(fileName);
+      readStream.pipe(res);
+      res.status(200)
+    })
+  }
 }

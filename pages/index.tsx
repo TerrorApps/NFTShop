e7219f438b0f22aaacfe16966ecefd41b2409d0c
Taka Goto <tgoto@8thlight.com>
2022-02-06 15:54:24 -0600

    add twitter tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,10 @@ export default function Home() {
   <div className="bg-gray-50">
   <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
     <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-      <span className="block">NFT Phone Wallpaper Regenerator</span>
+      <span className="block">NFT Image Regenerator</span>
       <span className="block text-indigo-600">Click on any project.</span>
       <span className="block text-indigo-600">Send any donations to BokuWaTaka.eth ❤️ </span>
+      <span className="block text-indigo-600">Follow me on Twitter: @BokuWaTakaE </span>
     </h2>
     <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
       <div className="inline-flex rounded-md shadow">

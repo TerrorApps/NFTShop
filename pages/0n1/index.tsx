993ec27b4f4ca917422e46418f783f4db90036d7
Taka Goto <tgoto@8thlight.com>
2022-02-06 21:17:40 -0600

    add home link

--- pages/0n1/index.tsx
+++ pages/0n1/index.tsx
@@ -19,6 +19,13 @@ export default function Home() {
   }
 `}</style>
     <div className="pt-4">
+        <div className="pb-4 text-green-500">
+            <a href="/">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                </svg>
+            </a>
+        </div>
         <Image className="pt-2" src="https://assets.website-files.com/60ef399b992671a8275e6cff/61cd2919c237ed3643da09b6_0n1logo.svg" alt="" width="72" height="72" />
     </div>
     <div className="pt-4">
--- pages/killergf/index.tsx
+++ pages/killergf/index.tsx
@@ -16,6 +16,13 @@ export default function Azuki() {
   }
 `}</style>
     <div className="pt-4 pl-4">
+        <div className="pb-4">
+            <a href="/">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                </svg>
+            </a>
+        </div>
       <img src="https://killergf.com/static/media/logo.c6f172e5.png" alt="" width="500" height="72" />
     </div>
   	<form className="m-4 flex"

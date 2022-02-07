d1c25140a40df22102833830aa75aea7e917a425
Taka Goto <tgoto@8thlight.com>
2022-02-06 21:44:27 -0600

    whitelist all domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['assets.website-files.com', "www.azuki.com", "herodev.mypinata.cloud"],
+    domains: [
+      'assets.website-files.com',
+      "www.azuki.com",
+      "herodev.mypinata.cloud",
+      "api.deadfellaz.io",
+      "api.uwucrew.art",
+      "gateway.pinata.cloud"
+    ],
   }
 }

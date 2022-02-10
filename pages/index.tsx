1ec035d6f8d74b93195acfcbaedb967dc4eccee8
Taka Goto <tgoto@8thlight.com>
2022-02-10 11:03:57 -0600

    remove long lost for now;

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,15 +65,6 @@ export default function Home() {
       </div>
     </div>
   </div>
-  <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-    <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-      <div className="ml-3 inline-flex rounded-md shadow">
-        <Link href="/longlost">
-          <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Long Lost</a>
-        </Link>
-      </div>
-    </div>
-  </div>
 </div>
   )
 }

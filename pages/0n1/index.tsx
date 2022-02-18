c1cd992467229477234a1240c84ad43144082980
Taka Goto <tgoto@8thlight.com>
2022-02-18 12:07:31 -0600

    dealing with previous links pointing to old design

--- pages/0n1/index.tsx
+++ pages/0n1/index.tsx
@@ -1,77 +1,21 @@
-import Head from 'next/head'
 import { useState } from 'react'
-import Image from 'next/image'
+import _ from "lodash"
+// import PhoneScreenList from '../../components/phone_screen_list'
+import dynamic from 'next/dynamic'
+
+const PhoneScreenListNoSSR = dynamic(() => import('../components/phone_screen_list'), {
+    ssr: false
+})
 
 export default function Home() {
-  const [value, setValue] = useState("value")
-  const [loading, setLoading] = useState(false)
-  const [failedFetch, setFailedFetch] = useState(false)
-  const [regenType, setRegenType] = useState("phonescreen")
-  const [regenTypString, setRegenTypeString] = useState("Phone Screen")
-  const [buttonColor, setButtonColor] = useState()
-
-  var image = null;
-  return (
-  <div className="pl-4">
-          <style jsx global>{`
-  body {
-    background: #000;
-  }
-`}</style>
-    <div className="pt-4">
-        <div className="pb-4 text-green-500">
-            <a href="/">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
-            </a>
+    return (
+        <div>
+        <PhoneScreenListNoSSR
+            name="0n1"
+            floor={1}
+            ceiling={7777}
+            cid="QmPSmLFF9aPM3CSU6WSTrtxobPGt6xxmLsEkNRo8K8YQZn"
+        />
         </div>
-        <Image className="pt-2" src="https://assets.website-files.com/60ef399b992671a8275e6cff/61cd2919c237ed3643da09b6_0n1logo.svg" alt="" width="72" height="72" />
-    </div>
-    <div className="pt-4">
-        <button 
-            onClick={() => { setRegenType("phonescreen"); setRegenTypeString("Phone Screen") }}
-            className="pt-4 pl-4 bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-green-700">
-            Phone Screen
-        </button>
-        <button 
-            onClick={() => { setRegenType("izzy"); setRegenTypeString("Izzy R0N1") }}
-            className="pt-4 pl-4 bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-green-700">
-            Izzy R0N1
-        </button>
-    </div>
-     (
-         <div>
-             <p className="pt-4 text-2xl text-green-500 ...">{ regenTypString }</p>
-            <form className="flex"
-                onSubmit={async (e: React.SyntheticEvent) => {
-                e.preventDefault();
-                const target = e.target as typeof e.target & {
-                    tokenid: { value: string };
-                };
-                const tokenId = target.tokenid.value;
-                setLoading(true)
-                setFailedFetch(false)
-                var res = await fetch(`/api/0n1/${regenType}?tokenId=${tokenId}`)
-                setLoading(false)
-                if (res.status == 400) {
-                    setFailedFetch(true)
-                } else {
-                    var blob = await res.blob()
-                    var urlCreator = window.URL || window.webkitURL
-                    setValue(urlCreator.createObjectURL(blob))
-                }
-            }}
-            >
-                <input type="text" className="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" name="tokenid" placeholder="Token ID" required autoFocus/>
-                <button className="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">{ loading ? "Thinking......." : "Press This Button" }</button>
-            </form>
-         </div>
-    <span className="pt-4 block text-green-500">Send any donations to BokuWaTaka.eth ❤️ </span>
-    <a href="https://twitter.com/BokuWaTakaE?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @BokuWaTakaE</a>
-    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-    { failedFetch ? <span className="pt-4 pl-4 block text-green-500"> Token ID is invalid. </span> : <div></div>}
-    {value != "value" && !loading && <img className="border-dashed border-2 border-sky-500 object-cover h-2532 w-1170 ..." src={value}></img>}
-</div>
-  );
+    )
 }
--- pages/azuki/index.tsx
+++ pages/azuki/index.tsx
@@ -1,58 +1,19 @@
-import Head from 'next/head'
-import { useState } from 'react'
 import Image from 'next/image'
-import Script from 'next/script'
+import { useState } from 'react'
+import { ImageLoaderProps } from 'next/image'
+import dynamic from 'next/dynamic'
 
-export default function Azuki() {
-  const [value, setValue] = useState("value")
-  const [loading, setLoading] = useState(false)
-  const [failedFetch, setFailedFetch] = useState(false)
-  var image = null;
-  return (
-  <div>
-          <style jsx global>{`
-  body {
-    background: #d31c3c;
-  }
-`}</style>
-    <div className="pt-4 pl-4">
-        <div className="pb-4">
-            <a href="/">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
-            </a>
-        </div>
-      <img src="https://www.azuki.com/Azuki%20Logo%20White.svg" alt="" width="500" height="72" />
-    </div>
-  	<form className="m-4 flex"
-      onSubmit={async (e: React.SyntheticEvent) => {
-        e.preventDefault();
-        const target = e.target as typeof e.target & {
-          tokenid: { value: string };
-        };
-        const tokenId = target.tokenid.value;
-        setLoading(true)
-        setFailedFetch(false)
-        var res = await fetch(`/api/azuki?tokenId=${tokenId}`)
-        setLoading(false)
-        if (res.status == 400) {
-            setFailedFetch(true)
-        } else {
-            var blob = await res.blob()
-            var urlCreator = window.URL || window.webkitURL
-            setValue(urlCreator.createObjectURL(blob))
-        }
-    }}
-    >
-    	<input type="text" className="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" name="tokenid" placeholder="Token ID" required autoFocus/>
-	    <button className="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">{ loading ? "Thinking......." : "Press This Button" }</button>
-	</form>
-    <span className="pt-4 pl-4 block text-slate-50">Send any donations to BokuWaTaka.eth ❤️ </span>
-    <a href="https://twitter.com/BokuWaTakaE?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @BokuWaTakaE</a>
-    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-    { failedFetch ? <span className="pt-4 pl-4 block text-white-500"> Token ID is invalid. </span> : <div></div>}
-    { value != "value" && !loading && <img className="object-cover h-2532 w-1170 ..." src={value} />}
-</div>
-  );
+const PhoneScreenListNoSSR = dynamic(() => import('../components/phone_screen_list'), {
+    ssr: false
+})
+
+export default function Home() {
+    return (
+        <PhoneScreenListNoSSR
+            name="azuki"
+            floor={0}
+            ceiling={1000}
+            cid="QmTVEzUdJiqFV5KeMwp4pgymTzaXcqZo5KTaHNsskuie76"
+        />
+    )
 }

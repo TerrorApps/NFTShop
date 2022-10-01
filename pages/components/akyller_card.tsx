1a3493480d0c40ab552ab6dbc21928f59a809bd2
Taka Goto <tgoto@8thlight.com>
2022-10-01 18:04:34 -0500

    fix card title

--- pages/components/akyller_card.tsx
+++ pages/components/akyller_card.tsx
@@ -3,7 +3,7 @@ export default function AkyllerCard() {
         <a href="/akyllers/phonescreen" className="rounded overflow-hidden shadow-lg">
         <img className="w-full" src="./home/akyllers_home.png" alt="Forest" />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Azuki</div>
+          <div className="font-bold text-xl mb-2">Akyllers</div>
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-indigo-600 mr-2 mb-2">#phone screen</span>
